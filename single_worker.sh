e01286a37f4397db608dd5f3f262aa4f484f8115
Marcos Monteiro <mrcsmonteiro@gmail.com>
2023-07-25 23:47:23 +1000

    Downgrade K8s

--- single_worker.sh
+++ single_worker.sh
@@ -5,21 +5,21 @@ apt-get -y install socat conntrack ipset
 #INSTALL BINARIES
 cd /home/cloud_user
 wget -q --show-progress --https-only --timestamping \
-  https://github.com/kubernetes-sigs/cri-tools/releases/download/v1.21.0/crictl-v1.21.0-linux-amd64.tar.gz \
+  https://github.com/kubernetes-incubator/cri-tools/releases/download/v1.0.0-beta.0/crictl-v1.0.0-beta.0-linux-amd64.tar.gz \
   https://storage.googleapis.com/gvisor/releases/release/latest/x86_64/runsc \
-  https://github.com/opencontainers/runc/releases/download/v1.0.0-rc93/runc.amd64 \
-  https://github.com/containernetworking/plugins/releases/download/v0.9.1/cni-plugins-linux-amd64-v0.9.1.tgz \
-  https://github.com/containerd/containerd/releases/download/v1.4.4/containerd-1.4.4-linux-amd64.tar.gz \
-  https://storage.googleapis.com/kubernetes-release/release/v1.21.0/bin/linux/amd64/kubectl \
-  https://storage.googleapis.com/kubernetes-release/release/v1.21.0/bin/linux/amd64/kube-proxy \
-  https://storage.googleapis.com/kubernetes-release/release/v1.21.0/bin/linux/amd64/kubelet
+  https://github.com/opencontainers/runc/releases/download/v1.0.0-rc5/runc.amd64 \
+  https://github.com/containernetworking/plugins/releases/download/v0.6.0/cni-plugins-amd64-v0.6.0.tgz \
+  https://github.com/containerd/containerd/releases/download/v1.1.0/containerd-1.1.0.linux-amd64.tar.gz \
+  https://storage.googleapis.com/kubernetes-release/release/v1.10.2/bin/linux/amd64/kubectl \
+  https://storage.googleapis.com/kubernetes-release/release/v1.10.2/bin/linux/amd64/kube-proxy \
+  https://storage.googleapis.com/kubernetes-release/release/v1.10.2/bin/linux/amd64/kubelet
 mkdir -p /etc/cni/net.d /opt/cni/bin /var/lib/kubelet /var/lib/kube-proxy /var/lib/kubernetes /var/run/kubernetes
 chmod +x kubectl kube-proxy kubelet runc.amd64 runsc
 mv runc.amd64 runc
 mv kubectl kube-proxy kubelet runc runsc /usr/local/bin/
-tar -xvf crictl-v1.21.0-linux-amd64.tar.gz -C /usr/local/bin/
-tar -xvf cni-plugins-linux-amd64-v0.9.1.tgz -C /opt/cni/bin/
-tar -xvf containerd-1.4.4-linux-amd64.tar.gz -C /
+tar -xvf crictl-v1.0.0-beta.0-linux-amd64.tar.gz -C /usr/local/bin/
+tar -xvf cni-plugins-amd64-v0.6.0.tgz -C /opt/cni/bin/
+tar -xvf containerd-1.1.0.linux-amd64.tar.gz -C /
 #CONTAINERD
 mkdir -p /etc/containerd/
 cat << EOF | tee /etc/containerd/config.toml
